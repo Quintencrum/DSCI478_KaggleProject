372c7634d8a384fb8b52af5984ca7b6e4ff9ae2f
Quintencrum <quintencrum@gmail.com>
2023-02-26 23:01:33 -0700

    Finally got my machine to run! :)

--- src/models/knn.py
+++ src/models/knn.py
@@ -1,1 +1,20 @@
 # KNN
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+print('Hello ------------------ ?????????????')
+
+class KNN:
+    def __init__(self, k):
+        self.k = k
+
+    def euclidean_dist(self, x1, x2):
+        return np.sqrt(np.sum((x1 - x2)**2))
+    
+    
+
+
