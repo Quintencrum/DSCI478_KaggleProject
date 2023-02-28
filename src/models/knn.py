4d8bdd36f73eb7946577ae25cbc66685f5984980
Quintencrum <quintencrum@gmail.com>
2023-02-28 11:06:51 -0700

    KNN (but it takes way too long to run 3:30+ hrs)

--- src/models/knn.py
+++ src/models/knn.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from collections import Counter
 
 import sys
 import os
@@ -27,18 +28,61 @@ class KNN:
 
     def euclidean_dist(self, x1, x2):
         return np.sqrt(np.sum((x1 - x2)**2))
+
+    def fit(self, x_train, y_train):
+        self.x_train = x_train.values
+        self.y_train = y_train.values
+
+    def predict(self, x_test):
+        x_test = x_test.values
+        y_pred = [self.check_dist_knn(x) for x in x_test]
+        return np.array(y_pred)
+
+    def check_dist_knn(self, x):
+        distances = [self.euclidean_dist(x, x_train) for x_train in self.x_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common_digit = Counter(k_nearest_labels).most_common(1)[0][0]
+        return most_common_digit
     
     
 
 
 def main():
     train = data.get_data("train")
+    # train_nump = train.to_numpy()
     # print(train)
     # print(train.dtypes)
-    print(train.iloc[0,0])  #[image num, label value or pixel value]
+    # print(train.iloc[0,0])  #[image num, label value or pixel value]
+    # print(train.iloc[0])
+    # print(type(train))
+
+    # print(train_nump[0])
+    # print(train.iloc[:,0])
+
+    y_train = train.iloc[:,0]
+    x_train = train.iloc[:,1:]
+
+    # data = pd.DataFrame({'X1': [1, 4, 7, 10], 'X2': [2, 5, 8, 11], 'X3': [3, 6, 9, 12], 'y': [0, 1, 1, 0]})
+    # X_train = data.iloc[:, :-1]
+    # y_train = data.iloc[:, -1]
+    # print(y_train)
+
+    knn = KNN(k=3)
+    knn.fit(x_train, y_train)
+
+
+    test = data.get_data("test")
+    x_test = test.iloc[:,:]
+
+
+    y_pred = knn.predict(x_test)
+
+    print(y_pred)
 
 
 
 if __name__ == '__main__':
     print("hello world")
+    print('')
     main()
